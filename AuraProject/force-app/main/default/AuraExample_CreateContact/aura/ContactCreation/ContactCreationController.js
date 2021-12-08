42beb7eb465e8c821a2cc361664cc0d40151beea
NabilBoudjellal <Boudjellalnabil@gmail.com>
2021-12-08 17:28:46 +0100

    demo create contact done

--- AuraProject/force-app/main/default/AuraExample_CreateContact/aura/ContactCreation/ContactCreationController.js
+++ AuraProject/force-app/main/default/AuraExample_CreateContact/aura/ContactCreation/ContactCreationController.js
@@ -1,13 +1,19 @@
 ({
     doSave : function(component, event, helper) {
-
+        
         var action = component.get("c.createContact");
 
         action.setParams({'contObj':component.get('v.contactObj')});
 
-        action.setCallback(this,function(data){
-            component.set('v.contactId',data.getReturnValue())
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+            component.set('v.contactId',response.getReturnValue());
+            }else {
+                alert('un probleme est survenu');
+            }
         });
         $A.enqueueAction(action);
+        
     }
 })
